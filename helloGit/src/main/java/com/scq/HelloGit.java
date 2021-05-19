b83d01f22b04dd236f2805192baab5a84945c68a
suochunqiang <1938500487@qq.com>
2021-05-19 23:19:19 +0800

    主干第七行添加

--- helloGit/src/main/java/com/scq/HelloGit.java
+++ helloGit/src/main/java/com/scq/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("分支添加");
+        System.out.println("主干第七行添加");
     }
 }
